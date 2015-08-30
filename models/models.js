fdbe06ee3edc451c79d99a56d823267ddeebffd0
ramanjaneyagupta <gupta.javacpp@gmail.com>
2015-08-30 02:37:25 -0500

    version1
    My sample app

---
+++ models/models.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    username: String,
+    password: String, //hash created from password
+    created_at: {type: Date, default: Date.now}
+});
+
+var postSchema = new mongoose.Schema({
+	text : String,
+	created_by : String,
+	created_at : {type: Date, default: Date.now}
+});
+
+mongoose.model("User",userSchema);
+mongoose.model("Post",postSchema);
